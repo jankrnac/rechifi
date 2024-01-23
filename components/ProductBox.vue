1d4ac0ab2110211ccbf84b021c01336c2b33f690
TorchSK <gtorch@gmail.com>
2024-01-23 16:50:40 +0100

    dd

--- components/ProductBox.vue
+++ components/ProductBox.vue
@@ -3,7 +3,7 @@
 
 <li :key="product.source" class="relative">
     <div class="relative aspect-square block w-full overflow-hidden rounded-lg hover:-translate-y-1 transition">
-        <nuxt-img :src="product.cover ? product.cover : 'iem.webp'" alt="" class="pointer-events-none object-cover" densities="x1" format="webp" sizes="100px lg:351px"/>
+        <nuxt-img :src="product.cover ? product.cover : 'iem.webp'" alt="" class="pointer-events-none object-cover" densities="x1" format="webp" sizes="200px md:300px lg:351px"/>
         <nuxt-link class="absolute inset-0" :to="product._path"></nuxt-link>
     </div>
 
