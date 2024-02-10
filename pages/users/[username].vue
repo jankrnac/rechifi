97e8def4a3e7f1246ed7cf97fb98d965156e4372
TorchSK <gtorch@gmail.com>
2024-02-10 09:07:36 +0100

    dd

--- pages/users/[username].vue
+++ pages/users/[username].vue
@@ -8,16 +8,28 @@
     <h2 class="font-bold text-2xl">Current setup</h2>
 
     <h2 class="font-bold text-2xl">Reviews</h2>
+    <div class="mx-auto lg:mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-app lg:grid-cols-4">
+        
+        <div v-for="review in reviews" class="relative">
+
+            <ReviewBox :review="review" />
+
+        </div>
+    </div>
 </div>
 
 </template>
 
 <script setup>
 
-const user = useSupabaseUser()
 const client = useSupabaseClient()
 
-const { data:profile } = await useFetch('/api/users/' + user.value.id)
+const { data:profile } = await client.from('profiles').select('*').eq('username',useRoute().params.username).single()
 
+const { data:reviews } = await useAsyncData(async () => {
+    const { data } = await client.from('reviews').select('*, profiles(username)').eq('profile_id', profile.id).order('created_at')
+
+    return data
+})
 
 </script>
