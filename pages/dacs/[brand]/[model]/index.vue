1542b6c97590d82361f1a866906c23ab4a5527bd
TorchSK <gtorch@gmail.com>
2024-06-04 15:04:53 +0200

    dd

--- pages/dacs/[brand]/[model]/index.vue
+++ pages/dacs/[brand]/[model]/index.vue
@@ -5,9 +5,10 @@
             <!-- Title -->
             <ProductsTitle :doc="doc">{{ doc.title }}</ProductsTitle>
     
-            <!-- Description -->
+            <!-- Description
             <ProductsDescription>{{ doc.description }}</ProductsDescription>
-    
+            -->
+
             <!-- Hero -->
             <ProductsHero v-if="doc.hero" :hero="doc.hero" />
     
@@ -19,7 +20,6 @@
     
             <!-- Features and Rating -->
             <div class="lg:flex flex-grow w-full max-w-app mb-24">
-                <ProductsFeatures v-if="doc.features" :features="doc.features" class="flex-grow mb-24 md:mb-0"/>
     
                 <div class="flex flex-grow items-center justify-center">
                     <ProductsOverallRating :rating="rating"/>
@@ -29,9 +29,10 @@
             <!-- Reviews -->
             <ProductsReviews class="mb-12 max-w-app"/>
     
-            <!-- Technicals -->
+            <!-- Technicals 
             <ProductsSpecs v-if="doc.specs && doc.specs.length" :doc="doc"/>
-    
+            -->
+
             <!-- Stores -->
             <ProductsStores v-if="doc.stores && doc.stores.length" :stores="doc.stores"/>
     
