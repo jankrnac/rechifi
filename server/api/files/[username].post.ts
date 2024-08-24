a962bfc4112871862ccdd777800782912b6dd4a0
TorchSK <gtorch@gmail.com>
2024-08-24 09:00:11 +0200

    gg

--- server/api/files/[username].post.ts
+++ server/api/files/[username].post.ts
@@ -15,34 +15,4 @@ export default eventHandler(async (event) =>
         return result;
     }
 
-
-    const S3 = new S3Client({
-        region: "auto",
-        endpoint: `https://7471c3f706c92aa33de7b01621143001.r2.cloudflarestorage.com`,
-        credentials: {
-            accessKeyId: '7269ddce39392ef6fa3dc42a5bb1353f',
-            secretAccessKey: '08027d84f624956e4c491c698b6801b891a7578ebf91d6cc61e8a09b6ddfd6d0',
-        },
-    });
-
-    const formData = await readMultipartFormData(event)
-
-    const filename = formData[0].filename
-
-    const random = generateString(12)
-    const ext = filename.split('.')[1]
-
-    const newFilename = random + '.' + ext
-
-    await S3.send(
-        new PutObjectCommand({
-            Body: formData[0].data,
-            Bucket: 'rechifi',
-            Key: username + '/' + newFilename,
-            ACL: "public-read",
-            ContentType:  formData[0].type,
-        })
-    )
-
-    return username + '/' + newFilename
 })
