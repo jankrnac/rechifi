6081e6588a6b4436728802b09062f6aa2d24bfbb
TorchSK <gtorch@gmail.com>
2024-10-02 17:46:40 +0200

    ff

--- app/components/elements/Options.vue
+++ app/components/elements/Options.vue
@@ -1,7 +1,24 @@
 <template>
 
 <div>
+    <UPopover>
 
+        <UButton icon="i-ph-gear" color="sky" />
+		
+		<template #panel>
+			<div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5">
+                	<div class="relative bg-white p-2 text-gray-800 text-left flex flex-col items-start space-y-5">
+						<div v-for="[key, value] of Object.entries(options)">
+						
+						</div>
+						
+                    	<UButton v-if="element.type != 'title'" color="red" size="xs" icon="i-ph-x" @click="$emit('delete')">
+                        	Delete
+						</UButton>
+					</div>
+				</div>
+			</template>
+	</UPopover>
 </div>
 </template>
   
