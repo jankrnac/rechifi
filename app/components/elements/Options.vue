75438df8bb35b558fcd282cb94cb1055f7976abf
TorchSK <gtorch@gmail.com>
2024-10-02 17:49:40 +0200

    ff

--- app/components/elements/Options.vue
+++ app/components/elements/Options.vue
@@ -9,7 +9,13 @@
 			<div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5">
                 	<div class="relative bg-white p-2 text-gray-800 text-left flex flex-col items-start space-y-5">
 						<div v-for="[key, value] of Object.entries(options)">
-						
+							<component 
+								:is="componentsMap[key]" 
+								:model="key"
+								:element="element"
+								:options="options"
+								@change="changed"
+							/>
 						</div>
 						
                     	<UButton v-if="element.type != 'title'" color="red" size="xs" icon="i-ph-x" @click="$emit('delete')">
