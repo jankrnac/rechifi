83a1b86eb4f02e9d10166b6606a9533795f107a5
TorchSK <gtorch@gmail.com>
2024-10-27 16:38:30 +0100

    ff

--- app/pages/upcoming/index.vue
+++ app/pages/upcoming/index.vue
@@ -1,14 +1,14 @@
 <template>
 
-    <div class="flex flex-col items-center">
+    <div class="flex flex-col items-center flex-1">
         <h1 class="text-3xl lg:text-6xl my-4 lg:mt-16 lg:mb-6">
-            <span class="font-bold">Announced & </span> 
+            <span class="font-bold">New & </span> 
             <span class="font-playfair">Upcoming</span>
         </h1>
     
         <Wave />
     
-        <ul v-if="upcomings.length" class="divide-y w-full max-w-app">
+        <ul v-if="upcomings.length" class="divide-y w-full max-w-[800px]">
     
             <template v-for="upcoming in upcomings" :key="upcoming._path">
                 <UpcomingBox :upcoming="upcoming" />
--- server/api/products/upcoming.get.ts
+++ server/api/products/upcoming.get.ts
@@ -1,7 +1,16 @@
+import { gte, lte, isNull } from 'drizzle-orm'
+
+
 export default eventHandler(async (event) => {
 
+    var d = Date.now() + -5*24*3600*1000;
+
     let products = await useDrizzle().query.products.findMany({
-        where: eq(tables.products.released, false)
+        where: or(
+            isNull(tables.products.releaseDate),
+            gte(tables.products.releaseDate, new Date().toISOString().split('T')[0]),
+            gte(tables.products.releaseDate, new Date(d).toISOString().split('T')[0]),
+        )
     })
 
     return products
